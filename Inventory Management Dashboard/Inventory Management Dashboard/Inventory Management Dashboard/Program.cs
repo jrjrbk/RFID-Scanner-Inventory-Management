fd403600e50cdb8d38f272377902aec5e67aa7fd
bernardj.kiminjr <juniorb3252@gmail.com>
2026-07-14 23:10:04 +0800

    Modified main method to use serial class

--- Inventory Management Dashboard/Inventory Management Dashboard/Inventory Management Dashboard/Program.cs
+++ Inventory Management Dashboard/Inventory Management Dashboard/Inventory Management Dashboard/Program.cs
@@ -1,5 +1,9 @@
+using System.Diagnostics;
+using Inventory_Management_Dashboard.Services;
+
 namespace Inventory_Management_Dashboard
 {
+    
     internal static class Program
     {
         /// <summary>
@@ -10,8 +14,19 @@ namespace Inventory_Management_Dashboard
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new Form1());
+            Console.WriteLine("helloworld");
+            
+            var RFIDReader = new SerialReader();
+            RFIDReader.RFIDDataReceived += (data) =>
+            {
+                Console.WriteLine($"SUCCESS - Data Received in Program.cs: {data}");
+            };
+            RFIDReader.StartReading("COM14", 9600);
+            Console.WriteLine("Press any key to stop");
+            Console.ReadKey();
+            RFIDReader.StopReading();
         }
     }
 }

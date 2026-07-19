0452937ff8acdc57ff45ae04fae76fab260593f6
bernardj.kiminjr <juniorb3252@gmail.com>
2026-07-19 16:39:31 +0800

    Data model for log entries visible in the winform

---
+++ Inventory Management Dashboard/Inventory Management Dashboard/Inventory Management Dashboard/Models/FormLogEntries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Dashboard.Models
+{
+    internal class FormLogEntries
+    {
+        public int logID { get; set; }
+        public string staffName { get; set; }
+        public string inventoryName { get; set; }
+        public string borrowedTime { get; set; }
+        public string returnedTime { get; set; }
+    }
+}

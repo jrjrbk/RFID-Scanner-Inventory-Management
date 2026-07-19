30ae33c09e97337132deb8293e9d2c366e8c929f
bernardj.kiminjr <juniorb3252@gmail.com>
2026-07-19 16:48:14 +0800

    Inventory List UI

---
+++ Inventory Management Dashboard/Inventory Management Dashboard/Inventory Management Dashboard/InventoryList.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Inventory_Management_Dashboard
+{
+    partial class InventoryList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 84);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(776, 354);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(56, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Add Item";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(648, 40);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Remove";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "Form4";
+            Text = "Form4";
+            Load += Form4_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Inventory Management Dashboard/Inventory Management Dashboard/Inventory Management Dashboard/InventoryList.cs
@@ -0,0 +1,51 @@
+﻿using Inventory_Management_Dashboard.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Inventory_Management_Dashboard
+{
+    public partial class InventoryList : Form
+    {
+        public InventoryList()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            var db = new DatabaseManager();
+            var inventoryItems = db.selectAllInventoryItems();
+            dataGridView1.DataSource = inventoryItems;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InventoryAddForm newWindow = new InventoryAddForm();
+            newWindow.ShowDialog();
+
+            var db = new DatabaseManager();
+            var inventoryItems = db.selectAllInventoryItems();
+            dataGridView1.DataSource = inventoryItems;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.SelectedRows.Count > 0) 
+            { 
+                int selectedID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+                var confirmResult = MessageBox.Show("Are you sure you want to delete this item?", "Confirm Delete", MessageBoxButtons.YesNo);
+                if(confirmResult == DialogResult.Yes)
+                {
+                    var db = new DatabaseManager();
+                    db.RemoveInventoryItem(selectedID);
+                    dataGridView1.DataSource = db.selectAllInventoryItems();
+                }
+            }
+        }
+    }
+}
